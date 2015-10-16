be6ecd7e4c0efa5474292e569ae2a9ef5584c099
Miguel Sosa <miguel.sosa.martinez@gmail.com>
2015-10-15 22:32:07 -0500

    Sorting LastNames from List of Objects
    Implementing Comparable to override compareTo method which will sort
    last names of a list of objects.

---
+++ collections/ComparatorLogic.java
@@ -0,0 +1,43 @@
+package oca.samples.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class ComparatorLogic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee objEmp = new Employee();
+		Employee objEmp2 = new Employee();
+		Employee objEmp3 = new Employee();
+		Employee objEmp4 = new Employee();
+
+		objEmp.setLastName("Sosa");
+		objEmp2.setLastName("Chavez");
+		objEmp3.setLastName("Martinez");
+		objEmp4.setLastName("Lopez");
+
+		List<Employee> empList = new ArrayList<Employee>();
+		empList.add(objEmp);
+		empList.add(objEmp2);
+		empList.add(objEmp3);
+		empList.add(objEmp4);
+
+		Collections.sort(empList);
+
+		Iterator<Employee> it = empList.iterator();
+
+		while (it.hasNext()) {
+			Employee obj = (Employee) it.next();
+			System.out.println(obj.getLastName());
+		}
+
+	}
+
+}
---
+++ collections/Employee.java
@@ -0,0 +1,28 @@
+package oca.samples.collections;
+
+public class Employee implements Comparable<Employee> {
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public int compareTo(Employee emp) {
+		return lastName.compareTo(emp.lastName);
+	}
+}
