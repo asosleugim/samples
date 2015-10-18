20a7d548dfc4e99d104d37476e059a06928cd33c
Miguel Sosa <miguel.sosa.martinez@gmail.com>
2015-10-17 19:18:41 -0500

    Factory Pattern

---
+++ designpatters/factorypattern/Circle.java
@@ -0,0 +1,9 @@
+package oca.samples.designpatters.factorypattern;
+
+public class Circle implements Shape{
+	  @Override
+	   public void draw() {
+	      System.out.println("Inside Circle::draw() method.");
+	   }
+	
+}
---
+++ designpatters/factorypattern/Shape.java
@@ -0,0 +1,7 @@
+package oca.samples.designpatters.factorypattern;
+
+public interface Shape {
+
+void draw();
+
+}
