f9269387ba4af2324887938e6bed1a158c8ceb57
Miguel Sosa <miguel.sosa.martinez@gmail.com>
2015-11-05 10:34:08 -0600

    Sorting Dates

--- collections/ComparatorLogic.java
+++ collections/ComparatorLogic.java
@@ -2,6 +2,7 @@ package oca.samples.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,11 +18,37 @@ public class ComparatorLogic {
 		Employee objEmp2 = new Employee();
 		Employee objEmp3 = new Employee();
 		Employee objEmp4 = new Employee();
+		
+		
+		long millisInDay = 60 * 60 * 24 * 1000;
+		long currentTime = new Date().getTime();
+		long dateOnly = (currentTime / millisInDay) * millisInDay;
+		Date clearDate = new Date(dateOnly);
+		
+		long millisInDay2 = 60 * 60 * 24 * 10020;
+		long currentTime2 = new Date().getTime();
+		long dateOnly2 = (currentTime2 / millisInDay2) * millisInDay2;
+		Date clearDate2 = new Date(dateOnly2);
+		
+		long millisInDay3 = 60 * 60 * 24 * 11080;
+		long currentTime3 = new Date().getTime();
+		long dateOnly3 = (currentTime3 / millisInDay3) * millisInDay3;
+		Date clearDate3 = new Date(dateOnly3);
 
 		objEmp.setLastName("Sosa");
+		objEmp.setBeginDate(clearDate);
+		objEmp.setEndDate(new Date());
 		objEmp2.setLastName("Chavez");
+		objEmp2.setBeginDate(clearDate2);
+		objEmp2.setEndDate(clearDate3);
 		objEmp3.setLastName("Martinez");
+		objEmp3.setBeginDate(clearDate3);
+		objEmp3.setEndDate(clearDate2);
 		objEmp4.setLastName("Lopez");
+		objEmp4.setBeginDate(new Date());
+		objEmp4.setEndDate(clearDate);
+		
+		
 
 		List<Employee> empList = new ArrayList<Employee>();
 		empList.add(objEmp);
@@ -33,11 +60,18 @@ public class ComparatorLogic {
 
 		Iterator<Employee> it = empList.iterator();
 
+		Date test = objEmp.getEndDate();
+		
+		
 		while (it.hasNext()) {
 			Employee obj =  it.next();
-			System.out.println(obj.getLastName());
+			System.out.println(obj.getBeginDate() );
+		
+			if(test.before(obj.getEndDate())){
+				test=obj.getEndDate();
+			}
 		}
-
+System.out.println("Last end date: " + test);
 	}
 
 }
--- collections/Employee.java
+++ collections/Employee.java
@@ -1,10 +1,37 @@
 package oca.samples.collections;
 
+import java.util.Date;
+
 public class Employee implements Comparable<Employee>
 {
 
 	private String firstName;
 	private String lastName;
+	private Date beginDate;
+	private Date endDate;
+
+	Employee(){
+		firstName = "";
+		lastName = "";
+		beginDate = new Date();
+		endDate = new Date();
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -24,6 +51,14 @@ public class Employee implements Comparable<Employee>
 
 	@Override
 	public int compareTo(Employee emp) {
-		return lastName.compareTo(emp.lastName);
+	
+		if ( beginDate.after(emp.beginDate) )
+	        return 1;
+	    else if ( beginDate.before(emp.beginDate) )
+	        return -1;
+
+	    else 
+	    	return 0;
+	
 	}
 }
